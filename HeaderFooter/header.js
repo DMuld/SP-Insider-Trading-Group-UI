6bcc95418030deba4b5eca9a43bbab49a53402a0
Devin <104116486+DMuld@users.noreply.github.com>
2023-05-02 19:12:33 -0500

    Changed  this to use correct terminology

--- HeaderFooter/header.js
+++ HeaderFooter/header.js
@@ -16,7 +16,7 @@ document.write('\
             </li>\
           </ul>\
           <div class="form-inline my-2 my-lg-0">\
-            <input class="form-control mr-sm-2" type="search" id="searchbox" placeholder="Search For Congress" aria-label="Search">\
+            <input class="form-control mr-sm-2" type="search" id="searchbox" placeholder="Search For Tickers" aria-label="Search">\
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="searchSubmit">Search</button>\
           </div>\
           <ul class = "navbar-nav">\
